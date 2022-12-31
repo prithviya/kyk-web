5cfbc66255f3d860c3281f5e3f391fcf410bc46a
prithviya <94467336+prithviya@users.noreply.github.com>
2022-12-31 14:28:15 +0530

    contetn updated in KYK activated grahas

--- src/component/Activated.js
+++ src/component/Activated.js
@@ -30,7 +30,39 @@ function Activated() {
                                 <li>Saturday – Sani, Rahu – Black or Dark blue</li>
                                 <li>Sunday – Suriyan – Orange or Red</li>
                             </ul>
-                            
+                            <div>
+                             <br/>   
+                            <button class="uk-button uk-margin-small-right clickhere" type="button" data-uk-toggle="target: #read_more">
+                                Read More
+                            </button>
+
+                                <div id="read_more" data-uk-modal>
+                                    <div class="uk-modal-dialog uk-modal-body uk-text-center uk-padding-large">
+                                        <h3 className='cust-head uk-text-left'>
+                                            KYK Calibrates your Life
+                                        </h3>
+                                        <p className='uk-text-justify'>
+                                            KYK does no magic but examines the position of the Grahas in your life and evaluates their positive or negative characteristics associating your problems over a period of time, hence activates or deactivates the Grahas that were predominantly getting you in trouble. So at KYK you are out of the tussle of performing any Pooja, Pariharam or go out of your way to set things right in your life. You carry on your daily routine with prayers and faith, at the same time practice certain guidelines or rules advised by the expert to activate or deactivate the Grahas. You are sure to witness the decline of your problems and a rejuvenating spirit in your life.
+                                        </p>
+                                        <br/>
+                                        <h3 className='cust-head uk-text-left'>
+                                            One-of-a-kind Astrology Experience
+                                        </h3>
+                                        <p className='uk-text-justify'>
+                                            KYK is one-stop-shop solution for all your queries, especially related to business, career and health. It creates a unique fusion of planetary features, astrology and science and hence is able to analyse your problems in practical and scientific aspects. It proves as a boon for those in business for this industry is one of the most unpredictable venture which puts you in great risk, distress and peril. But KYK is the best resort with regard to such issues as it proposes you the finest counsel to startup such business that will activate the Grahas which will take you to the path of prosperity, for example activating Sani Bhagavaan to do extremely well at iron business, Chandra Bhagavaan for  business related to catering industry, Dairy farming or Water treatment industry and Sukra Bhagavaan for Beauty  industry & Cosmetic trade, Garment & Apparels marketing or Confectionery marketing.
+                                        </p><br/>
+                                        <p className='uk-text-justify'>
+                                            As health is above all priority, KYK has incredible solutions for any health issues that makes you a cripple and slows down your life. Did you know that every part of our body is also controlled by these ethereal spheres, for example Sani Bhagavaan governs our legs, Suriya Bhagavaan our head and heart etc; and by taking care of them, we activate these Grahas to have a sound body and mind. KYK exactly does this by detecting the position of these Grahas, activates or deactivates them leading to a positive direction for a great wellbeing. 
+                                        </p><br/>
+                                        <p className='uk-text-justify'>
+                                            Since the known human civilization, humanity has sort the aid of astrology for its problems and the legacy has been followed till date. KYK adapts to both astrology and science of Grahas and hence the expert is able to guide you through your problems as well as reverse your life for a wonderful change.
+                                        </p>
+                                        <h3 className='cust-head'>
+                                            Know Your Karma is always at your service for your betterment.
+                                        </h3>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
--- src/component/about.js
+++ src/component/about.js
@@ -35,80 +35,80 @@ function about() {
                                     <h1 className='cust-head uk-margin-small uk-margin-top'>Recent <span>Updates</span></h1>
                                     <div class="uk-child-width-1-2@m uk-grid-small uk-grid-match" data-uk-grid>
                                         <div>
-                                                <div data-uk-slider='animation: push; autoplay:true; autoplay-interval: 3200; pause-on-hover: true;'>
-                                                    <ul className="uk-slider-items">                            
-                                                        <li className="uk-width-1-1">
-                                                            <div className="uk-card uk-card-default uk-padding uk-width-1-1@m">
-                                                                <b className='uk-margin-small'>Pradosham</b>
-                                                                <p className='uk-margin-small'>Date : 04-01-2023</p>
-                                                                <div class="uk-clearfix uk-text-center">
-                                                                    <div class="uk-float-left">
-                                                                        <a href={ POSTER_IMG_1 } download="Brochure.jpeg"  loading="lazy" className='custom-align-center' style={{textDecoration:'none'}}>
-                                                                            <button className='uk-button uk-margin-small-right' type='button'>
-                                                                                <img src="https://img.icons8.com/sf-regular/32/FFFFFF/downloading-updates.png" alt="download icon" style={{margin: 'auto'}}/>
-                                                                            </button>
-                                                                        </a>
-                                                                    </div>
-                                                                    <div class="uk-float-right">
-                                                                    <button class="uk-button uk-margin-small-right" type="button" data-uk-toggle="target: #modal-example"><img src="https://img.icons8.com/windows/32/FFFFFF/visible--v1.png" alt='view icon'/></button>
+                                            <div data-uk-slider='animation: push; autoplay:true; autoplay-interval: 3200; pause-on-hover: true;'>
+                                                <ul className="uk-slider-items">                            
+                                                    <li className="uk-width-1-1">
+                                                        <div className="uk-card uk-card-default uk-padding uk-width-1-1@m">
+                                                            <b className='uk-margin-small'>Pradosham</b>
+                                                            <p className='uk-margin-small'>Date : 04-01-2023</p>
+                                                            <div class="uk-clearfix uk-text-center">
+                                                                <div class="uk-float-left">
+                                                                    <a href={ POSTER_IMG_1 } download="Brochure.jpeg"  loading="lazy" className='custom-align-center' style={{textDecoration:'none'}}>
+                                                                        <button className='uk-button uk-margin-small-right' type='button'>
+                                                                            <img src="https://img.icons8.com/sf-regular/32/FFFFFF/downloading-updates.png" alt="download icon" style={{margin: 'auto'}}/>
+                                                                        </button>
+                                                                    </a>
+                                                                </div>
+                                                                <div class="uk-float-right">
+                                                                <button class="uk-button uk-margin-small-right" type="button" data-uk-toggle="target: #modal-example"><img src="https://img.icons8.com/windows/32/FFFFFF/visible--v1.png" alt='view icon'/></button>
 
-                                                                        <div id="modal-example" data-uk-modal>
-                                                                            <div class="uk-modal-dialog uk-modal-body uk-text-center">
-                                                                                <img src= {Image.POSTER_1}  width={'450px'} height={'450px'} alt='chakra_iamges' style={{margin: 'auto'}}/>                                                           
-                                                                            </div>
+                                                                    <div id="modal-example" data-uk-modal>
+                                                                        <div class="uk-modal-dialog uk-modal-body uk-text-center">
+                                                                            <img src= {Image.POSTER_1}  width={'450px'} height={'450px'} alt='chakra_iamges' style={{margin: 'auto'}}/>                                                           
                                                                         </div>
                                                                     </div>
                                                                 </div>
                                                             </div>
-                                                        </li>
-                                                        <li className='uk-width-1-1'>
-                                                            <div className="uk-card uk-card-default uk-padding uk-width-1-1@m">
-                                                                <b className='uk-margin-small'>Paurnami</b>
-                                                                <p className='uk-margin-small'>Date : 06-01-2023</p>
-    
-                                                            </div>
-                                                        </li>
-                                                        <li className='uk-width-1-1'>
-                                                            <div className="uk-card uk-card-default uk-padding uk-width-1-1@m">
-                                                                <b className='uk-margin-small'>Sankatahara Chaturthi</b>
-                                                                <p className='uk-margin-small'>Date : 10-01-2023</p>
-    
-                                                            </div>
-                                                        </li>
-                                                        <li className='uk-width-1-1'>
-                                                            <div className="uk-card uk-card-default uk-padding uk-width-1-1@m">
-                                                                <b className='uk-margin-small'>Shashthi</b>
-                                                                <p className='uk-margin-small'>Date : 13-01-2023</p>
-    
-                                                            </div>
-                                                        </li>
-                                                        <li className="uk-width-1-1">
-                                                            <div className="uk-card uk-card-default uk-padding uk-width-1-1@m">
-                                                                <b className='uk-margin-small'>Ashtami (Deipirai)</b>
-                                                                <p className='uk-margin-small'>Date : 14-01-2023</p>
-                                                                <div class="uk-clearfix uk-text-center">
-                                                                    <div class="uk-float-left">
-                                                                        <a href={ POSTER_IMG_1 } download="Brochure.jpeg"  loading="lazy" className='custom-align-center' style={{textDecoration:'none'}}>
-                                                                            <button className='uk-button uk-margin-small-right' type='button'>
-                                                                                <img src="https://img.icons8.com/sf-regular/32/FFFFFF/downloading-updates.png" alt='down icon'/>
-                                                                            </button>
-                                                                        </a>
-                                                                    </div>
-                                                                    <div class="uk-float-right">
-                                                                    <button class="uk-button uk-margin-small-right" type="button" data-uk-toggle="target: #modal-example"><img src="https://img.icons8.com/windows/32/FFFFFF/visible--v1.png" alt='show icon'/></button>
+                                                        </div>
+                                                    </li>
+                                                    <li className='uk-width-1-1'>
+                                                        <div className="uk-card uk-card-default uk-padding uk-width-1-1@m">
+                                                            <b className='uk-margin-small'>Paurnami</b>
+                                                            <p className='uk-margin-small'>Date : 06-01-2023</p>
 
-                                                                        <div id="modal-example" data-uk-modal>
-                                                                            <div class="uk-modal-dialog uk-modal-body uk-text-center">
-                                                                                <img src= {Image.POSTER_1}  width={'450px'} height={'450px'} alt='chakra_iamges' style={{margin: 'auto'}}/>                                                           
-                                                                            </div>
+                                                        </div>
+                                                    </li>
+                                                    <li className='uk-width-1-1'>
+                                                        <div className="uk-card uk-card-default uk-padding uk-width-1-1@m">
+                                                            <b className='uk-margin-small'>Sankatahara Chaturthi</b>
+                                                            <p className='uk-margin-small'>Date : 10-01-2023</p>
+
+                                                        </div>
+                                                    </li>
+                                                    <li className='uk-width-1-1'>
+                                                        <div className="uk-card uk-card-default uk-padding uk-width-1-1@m">
+                                                            <b className='uk-margin-small'>Shashthi</b>
+                                                            <p className='uk-margin-small'>Date : 13-01-2023</p>
+
+                                                        </div>
+                                                    </li>
+                                                    <li className="uk-width-1-1">
+                                                        <div className="uk-card uk-card-default uk-padding uk-width-1-1@m">
+                                                            <b className='uk-margin-small'>Ashtami (Deipirai)</b>
+                                                            <p className='uk-margin-small'>Date : 14-01-2023</p>
+                                                            <div class="uk-clearfix uk-text-center">
+                                                                <div class="uk-float-left">
+                                                                    <a href={ POSTER_IMG_1 } download="Brochure.jpeg"  loading="lazy" className='custom-align-center' style={{textDecoration:'none'}}>
+                                                                        <button className='uk-button uk-margin-small-right' type='button'>
+                                                                            <img src="https://img.icons8.com/sf-regular/32/FFFFFF/downloading-updates.png" alt='down icon'/>
+                                                                        </button>
+                                                                    </a>
+                                                                </div>
+                                                                <div class="uk-float-right">
+                                                                <button class="uk-button uk-margin-small-right" type="button" data-uk-toggle="target: #modal-example"><img src="https://img.icons8.com/windows/32/FFFFFF/visible--v1.png" alt='show icon'/></button>
+
+                                                                    <div id="modal-example" data-uk-modal>
+                                                                        <div class="uk-modal-dialog uk-modal-body uk-text-center">
+                                                                            <img src= {Image.POSTER_1}  width={'450px'} height={'450px'} alt='chakra_iamges' style={{margin: 'auto'}}/>                                                           
                                                                         </div>
                                                                     </div>
                                                                 </div>
                                                             </div>
-                                                        </li>
-                                                    </ul>
-                                                    <ul class="uk-slider-nav uk-dotnav uk-flex-center uk-margin"></ul>
-                                                </div>
+                                                        </div>
+                                                    </li>
+                                                </ul>
+                                                <ul class="uk-slider-nav uk-dotnav uk-flex-center uk-margin"></ul>
+                                            </div>
                                         </div>
                                         <div>
                                             <div data-uk-slider='animation: slide; autoplay:true; autoplay-interval: 2800; pause-on-hover: true;'>
